b4b280ca361908e6799cab43a039ef34609cb7b0
Raphael567 <raphaelcanuto464@gmail.com>
2025-08-16 18:07:42 -0300

    Feat: estilizar cards e pagina :sparkles:

--- SIME-web-dashboard/src/app/components/quadro-chamados/quadro-chamados.component.ts
+++ SIME-web-dashboard/src/app/components/quadro-chamados/quadro-chamados.component.ts
@@ -8,6 +8,10 @@ import { ChamadoCardComponent } from '../chamado-card/chamado-card.component';
   styleUrl: './quadro-chamados.component.css'
 })
 export class QuadroChamadosComponent {
+  @Input() imgAlta: string = '';
+  @Input() imgMedia: string = '';
+  @Input() imgBaixa: string = '';
+
   @Input() chamadosAlta: any[] = [];
   @Input() chamadosMedia: any[] = [];
   @Input() chamadosBaixa: any[] = [];
--- SIME-web-dashboard/src/app/pages/chamados-concluidos/chamados-concluidos.component.ts
+++ SIME-web-dashboard/src/app/pages/chamados-concluidos/chamados-concluidos.component.ts
@@ -2,14 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { chamadoProjection } from '../../DTOs/Projections/chamadoProjection';
 import { FuncionarioService } from '../../services/funcionario/funcionario.service';
+import { QuadroChamadosComponent } from '../../components/quadro-chamados/quadro-chamados.component';
 
 @Component({
   selector: 'app-chamados-concluidos',
-  imports: [RouterModule],
+  imports: [RouterModule, QuadroChamadosComponent],
   templateUrl: './chamados-concluidos.component.html',
   styleUrl: './chamados-concluidos.component.css'
 })
 export class ChamadosConcluidosComponent {
+
+  chamadosAlta = [
+    { data: '06/05/25', descricao: 'Computador quebrado', local: 'Lab. 2' }
+  ];
+
+  chamadosMedia = [
+    { data: '00/00/00', descricao: 'Nome', local: 'Local' }
+  ];
+
+  chamadosBaixa = [
+    { data: '00/00/00', descricao: 'Nome', local: 'Local' }
+  ];
 
   // chamadosConcluidos: chamadoProjection[] = [];
 
