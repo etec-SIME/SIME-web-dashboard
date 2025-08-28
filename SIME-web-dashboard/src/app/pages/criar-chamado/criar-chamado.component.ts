5bfd28fd76c8ce290b0d4c5fd88bcf19c36b10ce
Raphael567 <raphaelcanuto464@gmail.com>
2025-08-27 21:26:47 -0300

    Feat: Estruturar html e ts :sparkles:

--- SIME-web-dashboard/src/app/pages/criar-chamado/criar-chamado.component.ts
+++ SIME-web-dashboard/src/app/pages/criar-chamado/criar-chamado.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-chamado',
@@ -7,5 +8,42 @@ import { Component } from '@angular/core';
   styleUrl: './criar-chamado.component.css'
 })
 export class CriarChamadoComponent {
+  chamadoForm: FormGroup;
+  tiposChamado = [
+    { idTipoChamado: 1, nomeTipoChamado: 'Manutenção' },
+    { idTipoChamado: 2, nomeTipoChamado: 'Suporte' },
+    { idTipoChamado: 3, nomeTipoChamado: 'Outro' }
+  ];
+  selectedFile: File | null = null;
 
+  constructor(private fb: FormBuilder) {
+    this.chamadoForm = this.fb.group({
+      data: ['', Validators.required],
+      local: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      tipoChamado: ['', Validators.required],
+      problema: ['', Validators.required],
+      descricao: ['', Validators.required],
+      equipamento: [''],
+      codigoEquipamento: ['']
+    });
+  }
+
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+  }
+  
+  onSubmit() {
+    if (this.chamadoForm.valid) {
+      const formData = new FormData();
+      Object.keys(this.chamadoForm.value).forEach(key => {
+        formData.append(key, this.chamadoForm.value[key]);
+      });
+      if (this.selectedFile) {
+        formData.append('anexo', this.selectedFile);
+      }
+      console.log('Form enviado:', formData);
+      
+    }
+  }
 }
