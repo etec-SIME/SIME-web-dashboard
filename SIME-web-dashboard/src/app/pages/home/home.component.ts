24d38463868c1ad7aba77f6190a64ad1a097a0f6
Luiza2112 <ritafilhotes4@gmail.com>
2025-09-24 21:05:20 -0300

    :sparkles: Feat: Continuar estruturação dos cards na Home.

--- SIME-web-dashboard/src/app/pages/home/home.component.ts
+++ SIME-web-dashboard/src/app/pages/home/home.component.ts
@@ -10,6 +10,27 @@ import { CalendarioSemanalComponent } from "../../components/calendario-semanal/
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  opcaoAtual: 'salas' | 'labs' | 'outros' = 'salas';
 
+  salas = [
+    { nome: 'Sala 1', chamados: 1 },
+    { nome: 'Sala 2', chamados: 2 },
+    { nome: 'Sala 3', chamados: 3 },
+    { nome: 'Sala 4', chamados: 4 }
+  ];
+
+  labs = [
+    { nome: 'Lab 1', chamados: 1 },
+    { nome: 'Lab 2', chamados: 2 }
+  ];
+
+  outros = [
+    { nome: 'Biblioteca', chamados: 2 },
+    { nome: 'Auditório', chamados: 3 }
+  ];
+
+  setOpcao(opcao: 'salas' | 'labs' | 'outros') {
+    this.opcaoAtual = opcao;
+  }
 
 }
