c6eb82e342711b7f5ae40a4e8bec7de8f5fc3a8a
mari-mainarte <mariana.mainarte@etec.sp.gov.br>
2025-09-25 15:10:24 -0300

    :sparkles:Feat: git pull origin escola service

--- SIME-web-dashboard/src/app/pages/criar/criar.component.ts
+++ SIME-web-dashboard/src/app/pages/criar/criar.component.ts
@@ -1,6 +1,12 @@
 import { UsuarioProjection } from '../../DTOs/Projections/UsuarioProjection';
-import { UsuarioService } from './../../services/usuario/usuario.service';
-import { Component } from '@angular/core';
+import { EscolaService } from '../../services/escola/escola.service';
+import { ambienteRequestDTO } from '../../DTOs/ambienteRequestDTO';
+import { tipoChamadoRequestDTO } from '../../DTOs/tipoChamadoRequestDTO';
+import { tipoPerfilRequestDTO } from '../../DTOs/tipoPerfilRequestDTO';
+import { UsuarioService } from '../../services/usuario/usuario.service';
+import { departamentoRequestDTO } from '../../DTOs/departamentoRequestDTO';
+import { equipamentoRequestDTO } from '../../DTOs/equipamentoRequestDTO';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-criar',
@@ -8,23 +14,54 @@ import { Component } from '@angular/core';
   templateUrl: './criar.component.html',
   styleUrl: './criar.component.css'
 })
-export class CriarComponent {
+export class CriarComponent implements OnInit{
 
+  locais: ambienteRequestDTO[] = [];
+  tiposChamado: tipoChamadoRequestDTO[] = [];
+  tiposPerfis: tipoPerfilRequestDTO[] = [];
   perfis: UsuarioProjection[] = [];
-  qtdPerfis: number = 0;
+  departamentos: departamentoRequestDTO[] = [];
+  equipamentos: equipamentoRequestDTO[] = [];
 
-  constructor(private usuarioService: UsuarioService) {}
+  qtdLocais: number = 0;
+  qtdTiposChamado: number = 0;
+  qtdTipoPerfis: number = 0;
+  qtdPerfis: number = 0;
+  qtdDepartamentos: number = 0;
+  qtdEquipamentos: number = 0;
+
+  constructor(private escolaService: EscolaService, private usuarioService: UsuarioService) {}
 
   ngOnInit(): void {
-    this.carregarElemeentos();
+    this.carregarElementos();
   }
 
-  carregarElemeentos(): void{
+  carregarElementos(): void{
+
+    this.escolaService.getAllAmbiente()
+      .subscribe(resp => this.locais = resp);
+
+    this.escolaService.getAllTipoChamado()
+      .subscribe(resp => this.tiposChamado = resp);
+
+    this.escolaService.getAllTipoPerfil()
+      .subscribe(resp => this.tiposPerfis = resp);
+
     this.usuarioService.getAllUsuarios()
-    .subscribe(
-      resp => this.perfis = resp
-    );
-    this.qtdPerfis = this.perfis.length;
-  }
+      .subscribe(resp => this.perfis = resp);
 
+    this.escolaService.getAllDepartamento()
+      .subscribe(resp => this.departamentos = resp);
+
+    this.escolaService.getAllEquipamento()
+      .subscribe(resp => this.equipamentos = resp);
+
+    this.qtdTiposChamado = this.tiposChamado.length;
+    this.qtdTipoPerfis = this.tiposPerfis.length;
+    this.qtdPerfis = this.perfis.length;
+    this.qtdDepartamentos =  this.departamentos.length;
+    this.qtdEquipamentos = this.equipamentos.length;
+
+    console.log(this.locais);
+  }
 }
