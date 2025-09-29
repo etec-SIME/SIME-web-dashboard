8b82234c57135035e4f1ae472fbf2eb30a8a1c1a
Raphael Canuto <113649023+Raphael567@users.noreply.github.com>
2025-09-29 20:55:35 -0300

    Revert ":sparkles: Feat: Terminar de ajustar a posição dos elementos da Home e ativar os botões de exibição dos cards."

--- SIME-web-dashboard/src/app/app.config.ts
+++ SIME-web-dashboard/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,20 +7,11 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './services/auth/interceptor/auth.interceptor';
 //import { provideHttpClient, withNoHttpTransferCache } from '@angular/common/http';
 
-import { CalendarModule, DateAdapter } from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
-
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideHttpClient(withInterceptors([authInterceptor])),
-    importProvidersFrom(
-      CalendarModule.forRoot({
-        provide: DateAdapter,
-        useFactory: adapterFactory
-      })
-    )
   ]
 };
