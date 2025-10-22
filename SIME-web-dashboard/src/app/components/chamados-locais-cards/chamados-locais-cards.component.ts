aaa34d84bcbdf81394a3923da101e20f354462b6
Luiza2112 <ritafilhotes4@gmail.com>
2025-10-22 19:07:10 -0300

    Revert ":sparkles: Feat: Iniciar o css dos cards de chamados filtrados."
    This reverts commit bdfa3ef20bd362e637888b78efc052616d7307e8.

--- SIME-web-dashboard/src/app/components/chamados-locais-cards/chamados-locais-cards.component.ts
+++ SIME-web-dashboard/src/app/components/chamados-locais-cards/chamados-locais-cards.component.ts
@@ -6,6 +6,7 @@ import { TipoChamadoSelectDTO } from '../../DTOs/TipoChamadoSelectDTO';
 import { AmbienteSelectDTO } from '../../DTOs/AmbienteSelectDTO';
 import { chamadosAmbienteDTO } from '../../DTOs/chamadosAmbienteDTO';
 
+
 @Component({
   selector: 'app-chamados-locais-cards',
   standalone: true,
@@ -25,22 +26,19 @@ export class ChamadosLocaisCardsComponent {
     }
 
     return this.chamados.map(chamado => ({
-        /*const tipo = this.tiposChamado.find(t =>
+        /*const tipo = this.tiposChamado.find(t => 
           Number(t.idTipoChamado) === Number(chamado.TipoChamado.idTipoChamado));
-
+        
         return {};*/
           ...chamado,
           dataAberturaFormatada: this.formatarData(chamado.dataAbertura),
           nomeTipoChamado: chamado.tipoChamado?.nomeTipoChamado || "Tipo do Chamado não encontrado."
-
+        
       }));
   }
 
   public formatarData(data: string): string {
-    if (!data){
-      console.log("Data vazia");
-      return '';
-    }
+    if (!data) return '';
     return data.split('T')[0].split('-').reverse().join('/');
   }
 
