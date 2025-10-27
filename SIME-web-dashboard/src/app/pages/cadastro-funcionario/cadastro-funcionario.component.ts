9706cb8372a737aca38bdab8262bac2296f9db03
PaolaCondori <paola.condori@etec.sp.gov.br>
2025-10-27 19:46:01 -0300

    :sparkles:Feat: Página de cadastro quase finalizada

--- SIME-web-dashboard/src/app/pages/cadastro-funcionario/cadastro-funcionario.component.ts
+++ SIME-web-dashboard/src/app/pages/cadastro-funcionario/cadastro-funcionario.component.ts
@@ -2,18 +2,37 @@ import { Component } from '@angular/core';
 import { EscolaService } from '../../services/escola/escola.service';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { usuarioRequestDTO } from '../../DTOs/usuarioRequestDTO';
+import { TipoPerfilResponseDTO } from '../../DTOs/TipoPerfilResponseDTO';
+import { PublicService } from '../../services/public-routes/public.service';
+import { AuthService } from '../../services/auth/auth.service';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cadastro-funcionario',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterModule, CommonModule],
   templateUrl: './cadastro-funcionario.component.html',
   styleUrl: './cadastro-funcionario.component.css'
 })
 export class CadastroFuncionarioComponent {
   formCadastrarUsuario!: FormGroup;
-  constructor(private escolaService: EscolaService, private fb: FormBuilder){
+  constructor(
+    private escolaService: EscolaService,
+     private fb: FormBuilder,
+     private authService: AuthService,
+     private publicService: PublicService
+    ){
+    this.loadSelectTipoPerfil();
     this.cadastrarUsuario();
+    this.formCadastrarUsuario = this.fb.group({
+      tipoPerfil: [this.tiposPerfil[0] || null],
+      codigoEtec: ['', Validators.required],
+      senha: ['', Validators.required],
+    });
   }
+  tiposPerfil: TipoPerfilResponseDTO[] = [];
+   showPassword = false;
+   
 
   cadastrarUsuario(){
     this.formCadastrarUsuario =  this.fb.group({
@@ -47,6 +66,21 @@ export class CadastroFuncionarioComponent {
         console.log(err)
       }
     })
+  }
+
+    loadSelectTipoPerfil() {
+    this.publicService.getTipoPerfilNomes().subscribe({
+      next: (tiposPerfil) => {
+        const tipoPadrao: TipoPerfilResponseDTO = { idTipoPerfil: 0, nomeTipoPerfil: 'Escola' };
+  
+        this.tiposPerfil = tiposPerfil.some(t => t.idTipoPerfil === tipoPadrao.idTipoPerfil)
+          ? tiposPerfil
+          : [tipoPadrao, ...tiposPerfil];
+  
+        this.formCadastrarUsuario.get('tipoPerfil')?.setValue(this.tiposPerfil[0]);
+      },
+      error: (err) => console.error('Erro ao buscar tipos de perfil: ', err),
+    });
   }
 
 
